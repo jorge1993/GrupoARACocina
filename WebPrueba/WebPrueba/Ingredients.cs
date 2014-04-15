8a880799b932be727af12b60ab262e8bab2e168e
Alberto15 <alberto-esteban@hotmail.es>
2014-04-15 14:18:50 +0200

    Entidades HADA
    Entidades de la practica

---
+++ WebPrueba/WebPrueba/Ingredients.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebPrueba
+{
+    public class Ingredients
+    {
+        private string name;
+        private string description;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+
+    }
+}
---
+++ WebPrueba/WebPrueba/User.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebPrueba
+{
+    public class User
+    {
+        private string email;
+        private string name;
+        private string surname;
+        private string country;
+        private int age;
+        private string nick;
+        private string password;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+ 
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+    
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+       
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+       
+
+        public string Nick
+        {
+            get { return nick; }
+            set { nick = value; }
+        }
+       
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}

4e05259772566fc0cc3bc01a0e4d0ebbd625f4dd
Jorge Vergara Roa <jvr27@alu.ua.es>
2014-04-15 14:51:39 +0200

    EN finish
    aslñdkfjñas

---
+++ WebPrueba/WebPrueba/BooksEN.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebPrueba
+{
+    public class Books
+    {
+        private string title;
+        private string author;
+        private long isbn;
+        private int price;
+        private int publication_date;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public long Isbn
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
+       
+    
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+      
+
+        public int Publication_date
+        {
+            get { return publication_date; }
+            set { publication_date = value; }
+        }
+
+    }
+}
---
+++ WebPrueba/WebPrueba/RecipeEN.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebPrueba
+{
+    public class Recipe
+    {
+        private long id;
+        private string name;
+        private int time;
+        private string subtitle;
+        private string description;
+        private string video;
+        private string origin;
+        private string difficulty;
+
+        public long Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+
+        public int Time
+        {
+            get { return time; }
+            set { time = value; }
+        }
+        
+
+        public string Subtitle
+        {
+            get { return subtitle; }
+            set { subtitle = value; }
+        }
+        
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        
+        public string Video
+        {
+            get { return video; }
+            set { video = value; }
+        }
+
+
+
+        public string Origin
+        {
+            get { return origin; }
+            set { origin = value; }
+        }
+      
+        public string Difficulty
+        {
+            get { return difficulty; }
+            set { difficulty = value; }
+        }
+
+
+
+    }
+}

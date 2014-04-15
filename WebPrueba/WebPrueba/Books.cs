a564c06aefed84a221a60a87216ad62cdd3bf2a4
Alberto15 <alberto-esteban@hotmail.es>
2014-04-15 14:27:17 +0200

    Nueva version entidades
    nanan

---
+++ WebPrueba/WebPrueba/Books.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebPrueba
+{
+    public class Books
+    {
+        private string title;
+        private string author;
+        private int ISBN;
+
+    }
+}
---
+++ WebPrueba/WebPrueba/Utensils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebPrueba
+{
+    public class Utensils
+    {
+        private string name;
+        private string material;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+       
+        public string Material
+        {
+            get { return material; }
+            set { material = value; }
+        }
+
+
+    }
+}

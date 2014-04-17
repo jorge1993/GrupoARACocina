372ed5aa6f2cbfa2008a47fa8c7973c1be4c1808
cgmora12 <cesar_realmadrid_12@hotmail.com>
2014-04-17 17:17:50 +0200

    Cad
    Cad & EN of book

--- WebPrueba/WebPrueba/BooksEN.cs
+++ WebPrueba/WebPrueba/BooksEN.cs
@@ -5,45 +5,90 @@ using System.Web;
 
 namespace WebPrueba
 {
-    public class Books
+    public class BooksEN
     {
-        private string title;
-        private string author;
-        private long isbn;
-        private int price;
-        private int publication_date;
+        //Default constructor without parameters
+        public BooksEN()
+        {
+            title = "";
+            author = "";
+            isbn = "";
+            price = 0;
+            date = "";
+        }
 
-        public string Title
+        //Constructor only with the title (PK)
+        public BooksEN(String t)
+        {
+            title = t;
+            author = "";
+            isbn = "";
+            price = 0;
+            date = "";
+        }
+
+        //Ctor with all the attributes
+        public BooksEN(String t, String a, String i, int p, string d)
+        {
+            title = t;
+            author = a;
+            isbn = i;
+            price = p;
+            date = d;
+        }
+
+        //Function to add a new book
+        public void insertBook(BookEN book)
+        {
+        }
+
+        //Function to remove a book
+        public void deleteBook()
+        {
+        }
+
+        //Modify the price of a book
+        public void changePrice()
+        {
+        }
+
+        // Getter/Setter of title
+        public String Title
         {
             get { return title; }
             set { title = value; }
         }
 
-        public string Author
+        // Getter/Setter of author
+        public String Author
         {
             get { return author; }
             set { author = value; }
         }
 
-        public long Isbn
+        // Getter/Setter of isbn
+        public String Isbn
         {
             get { return isbn; }
             set { isbn = value; }
         }
-       
-    
-        public int Price
-        {
-            get { return price; }
-            set { price = value; }
-        }
-      
 
-        public int Publication_date
+        // Getter/Setter of date
+        public String Date
         {
-            get { return publication_date; }
-            set { publication_date = value; }
+            get { return date; }
+            set { date = value; }
         }
 
+        /***********/
+        /** DATOS **/
+        /***********/
+        private String title;
+        private String author;
+        private String isbn;
+        private int price;
+        private String date;
+
+        private BooksCAD booksCAD;
     }
 }
